747caf5d9d9cb558762b4c3fbaf3a7bfbc28f6db
HyeonggyuKang <kin0204@naver.com>
2025-07-20 21:58:34 +0900

    내림차순 정렬추가

--- learn_basic_gramer/src/learn01/basic01/SortExample.java
+++ learn_basic_gramer/src/learn01/basic01/SortExample.java
@@ -8,11 +8,38 @@ public class SortExample {
         int[] arr = {5, 3, 1, 4};
         Arrays.sort(arr); // 오름차순 정렬
 
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+        	System.out.println(arr[i]);
+        }
+        
         // 리스트 정렬
         ArrayList<Integer> list = new ArrayList<>();
         list.add(5);
         list.add(1);
         list.add(3);
         Collections.sort(list); // 오름차순 정렬
+        
+        
+
+
+        for(int i = 0; i < list.size(); i++)
+        {
+        	System.out.println(list.get(i));
+        }
+        
+
+        // 내림차순 정렬
+        list.sort(Collections.reverseOrder());
+        
+
+
+        for(int i = 0; i < list.size(); i++)
+        {
+        	System.out.println(list.get(i));
+        }
+        
+        
     }
 }
