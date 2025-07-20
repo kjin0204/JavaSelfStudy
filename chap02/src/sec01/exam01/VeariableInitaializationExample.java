d8014dcb18f0223280fb311f95416ebcbc781a1f
HyeonggyuKang <kin0204@naver.com>
2025-07-20 21:13:21 +0900

    chap02 추기

---
+++ chap02/src/sec01/exam01/VeariableInitaializationExample.java
@@ -0,0 +1,14 @@
+package sec01.exam01;
+
+public class VeariableInitaializationExample {
+
+	public static void main(String[] args) {
+		
+		int value = 30;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}
---
+++ chap02/src/sec01/exam02/VariableUseExample.java
@@ -0,0 +1,12 @@
+package sec01.exam02;
+
+public class VariableUseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = -128;
+		var test = 2;
+		
+	}
+
+}
